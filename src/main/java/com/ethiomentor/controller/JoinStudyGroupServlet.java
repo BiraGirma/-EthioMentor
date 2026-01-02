349ea7b544ef5a77bf03902dea8ac8bdcd00d0a0
Beniam Lemma <benonforever@gmail.com>
2026-01-02 12:36:45 +0300

    Add study group controllers and messaging DAO
    Signed-off-by: Beniam Lemma <benonforever@gmail.com>

---
+++ src/main/java/com/ethiomentor/controller/JoinStudyGroupServlet.java
@@ -0,0 +1,36 @@
+package com.ethiomentor.controller;
+
+import com.ethiomentor.dao.StudyGroupDAO;
+import com.ethiomentor.model.User;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/studygroups/join")
+public class JoinStudyGroupServlet extends HttpServlet {
+    private final StudyGroupDAO dao = new StudyGroupDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        try {
+            User user = (User) req.getSession().getAttribute("user");
+            if (user == null) {
+                resp.setStatus(401);
+                resp.getWriter().write("{\"error\":\"User not logged in\"}");
+                return;
+            }
+
+            int groupId = Integer.parseInt(req.getParameter("groupId"));
+            dao.joinGroup(groupId, user.getId());
+            int count = dao.getMembersCount(groupId);
+
+            resp.getWriter().write("{\"membersCount\":" + count + "}");
+        } catch (Exception e) {
+            resp.setStatus(500);
+            resp.getWriter().write("{\"membersCount\":0}");
+            e.printStackTrace();
+        }
+    }
+}
